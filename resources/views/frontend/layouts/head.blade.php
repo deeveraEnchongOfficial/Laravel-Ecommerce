a4b5a51b379c5d7429a948f29eb0c23720eb8f19
Enchong <deveraflorencio18@gmail.com>
2024-07-21 07:36:59 +0800

    dockerizePart4

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,5 +1,4 @@
 <!-- Meta Tag -->
-@yield('meta')
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
