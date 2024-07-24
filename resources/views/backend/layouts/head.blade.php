051d2217490668814079fb915d193c37a07ab2e9
Enchong <deveraflorencio18@gmail.com>
2024-07-24 23:27:39 +0800

    dockerizePart3-2

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -1,6 +1,6 @@
 <head>
 
-    <meta charset="utf-8">
+    {{-- <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> --}}
     <link rel="icon" href="{{ URL::asset('/images/icon/Padilla_gowns_new.png') }}" type="image/x-icon"/>
     <title>Padilla Gowns and Barongs || DASHBOARD</title>
 
