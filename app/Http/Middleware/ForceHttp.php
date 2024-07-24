2cc24a3d0138e23573f3115bd4011b2c525eb966
Enchong <deveraflorencio18@gmail.com>
2024-07-24 23:10:31 +0800

    dockerizePart3-2

---
+++ app/Http/Middleware/ForceHttp.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceHttp
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->secure()) {
+            return redirect()->secure($request->path());
+        }
+
+        return $next($request);
+    }
+}
